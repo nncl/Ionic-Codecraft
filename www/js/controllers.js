43f67179ee277fade33d944633a27c961272f913
Cauê <nncl@live.com>
2015-10-27 20:30:14 -0200

    Data binding stuff

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,10 @@
-angular.module('starter.controllers', [])
+var app = angular.module('starter.controllers', []);
 
-.controller('DashCtrl', function($scope) {})
+app.controller('DashCtrl', function($scope) {
+  $scope.title = "Moooo....";
+})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+app.controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -17,11 +19,11 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+app.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
+app.controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
   };
